37d8531b0015334c2e422b553c2278af928fb232
arsenijlevin <arsenij.levin19@gmail.com>
2022-11-15 05:13:42 +0300

    Printing 20 services to the page

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,7 +1,75 @@
+import { GetServerSideProps } from 'next';
 import React, { Component } from 'react';
 
-export default class Services extends Component {
+import { PrismaClient } from '@prisma/client';
+import Table from '../components/table/Table';
+const prisma = new PrismaClient();
+
+type Props = {
+  server: {
+    databaseData: string[][];
+  };
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const servicesData = await prisma.rendered_services.findMany({
+    orderBy: {
+      date_time: 'desc'
+    },
+    take: 20,
+    include: {
+      doctors: {
+        select: {
+          name: true,
+          surname: true,
+          patronymic: true
+        }
+      },
+      patients: {
+        select: {
+          name: true,
+          surname: true,
+          patronymic: true
+        }
+      },
+      services: {
+        select: {
+          title: true
+        }
+      }
+    }
+  });
+
+  return {
+    props: {
+      server: {
+        databaseData: servicesData.map(service => [
+          `${service.date_time.toLocaleString()}`,
+          `${service.doctors.surname} ${service.doctors.name} ${service.doctors.patronymic}`,
+          `${service.patients.surname} ${service.patients.name} ${service.patients.patronymic}`,
+          `${service.services.title}`,
+          `${service.result}`
+        ])
+      }
+    }
+  };
+};
+
+export default class Services extends Component<Props> {
   render() {
-    return <div>services</div>;
+    console.log(this.props.server.databaseData);
+
+    return (
+      <Table
+        data={this.props.server.databaseData}
+        headerTitles={[
+          'Дата и время',
+          'Врач',
+          'Пациент',
+          'Услуга',
+          'Результат'
+        ]}
+      ></Table>
+    );
   }
 }
